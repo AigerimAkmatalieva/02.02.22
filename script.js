41895efad6af6b956b16c4de9c9d6a32d82d0e83
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-02-02 14:35:38 +0600

    JavaScript

--- script.js
+++ script.js
@@ -9,3 +9,30 @@ var exampleNumberNumber3 = Infinity;
 var exampleNumber4 = NaN;
 
 document.write(typeofexampleNumber1);
+
+
+//string
+var exampleString1 = "Hello world";
+var exampleStrin2 = 'Hello'
+var exampleString3 = "4"
+
+
+//Boolean 
+var exampleBoolean1 = true;
+var exampleBoolean2 = false;
+
+
+//Special types
+var exampleNull = null; //пустая
+var exampleUndefined = undefined; //не задали
+
+//BigInt-Not now
+var exampleBigInt = 385783892010174884n;
+
+//Symbol- Not Now
+var exampleSymbol = Symbol("id")
+
+//Objects
+var exampleObject1 = {};
+var exampleObject2 = Math;
+document.write(typeof exampleObject)
