b28132f998f296124541ec9f69c539f0d70a66db
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-02-02 14:29:58 +0600

    JavaScript

---
+++ script.js
@@ -0,0 +1,11 @@
+var message = "Hello world";
+var emptyVAriable;
+var firstName, lastName = "Akmatalieva";
+
+//Number
+var exampleNumber = 123;
+var exampleNumber2 = 123.45;
+var exampleNumberNumber3 = Infinity;
+var exampleNumber4 = NaN;
+
+document.write(typeofexampleNumber1);
